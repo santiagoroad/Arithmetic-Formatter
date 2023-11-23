f2df41fde2a7f35f40ecc1dd013a7af7377b0a9b
santiagoroad <santiagoroa5@gmail.com>
2023-11-23 13:46:41 -0500

    Restar again

--- main.py
+++ main.py
@@ -3,20 +3,9 @@ from unittest import main
 
 from arithmetic_arranger import arithmetic_arranger
 
-# Error: Too many problems.
-# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "123 + 49", "123 + 49", "123 + 49"]))
 
-# Error: Operator must be + or - .
-# print(arithmetic_arranger(["32 + 698", "3801 / 2", "45 * 43", "123 + 49"]))
-
-# Error: Numbers cannot be more than four digits.
-# print(arithmetic_arranger(["32234 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-
-# Error: Numbers must only contain digits.
-# print(arithmetic_arranger(["3g2 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-
-# Good
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
 
 # Run unit tests automatically
 main(['-vv'])
