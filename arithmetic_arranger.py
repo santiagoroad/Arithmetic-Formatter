cd1979a07124c3b01cd94e8cf130544d58cb10c5
santiagoroad <santiagoroa5@gmail.com>
2023-11-23 23:18:49 -0500

    Optimazing code, finalizing activity with the test corrects

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,38 @@
-def arithmetic_arranger(problems):
-    arranged_problems = ''
+def arithmetic_arranger(problems, showAnswer = False):
+    arranged_problems = ""
+    firstLine = ""
+    secondLine = ""
+    separator = ""
+    answer = ""
+
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    
+    for problem in problems:
+        number1, operation, number2 = problem.split()
+
+        if operation not in ('+', '-'):
+            return "Error: Operator must be '+' or '-'."
+        
+        if number1.isdigit() is not True or number2.isdigit() is not True:
+            return "Error: Numbers must only contain digits."
+        
+        if len(number1) > 4 or len(number2) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        
+        lengthDiference = max(len(number1), len(operation + " " + number2))
+
+        if showAnswer:
+            if operation == "+":
+                answer += str(int(number1) + int(number2)).rjust(lengthDiference) + "    "
+            else:
+                answer += str(int(number1) - int(number2)).rjust(lengthDiference) + "    "
+        
+
+        firstLine += number1.rjust(lengthDiference) + "    "
+        secondLine += operation + " " + number2.rjust(lengthDiference-2) + "    "
+        separator += "-" * (lengthDiference) + "    "
+
+    arranged_problems = firstLine + "\n" + secondLine + "\n" + separator + "\n" + answer
 
     return arranged_problems
--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from unittest import main
 
 from arithmetic_arranger import arithmetic_arranger
 
+# Error: Too many problems
+# print(arithmetic_arranger(['44 + 815', '909 - 2', '45 + 43', '123 + 49','888 + 40', '653 + 87']))
 
+# print(arithmetic_arranger(["32 * 698", "3801 / 2", "45 + 43", "123 + 49"]))
+
+# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+print("aaaaaaaaaaaaaaaaaaa")
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-
 
 # Run unit tests automatically
 main(['-vv'])
